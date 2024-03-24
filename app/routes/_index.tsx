d4241a2ae0a0a0f82e7f09d4435d4c0c8ebcde43
mopeneko <mopeneko@lem0n.cc>
2024-03-24 18:31:16 +0900

    Remove unused import

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import type { MetaFunction } from '@remix-run/node';
 import { useState } from 'react';
 
 export const meta: MetaFunction = () => {
