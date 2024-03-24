ef3a24f5bca2be7c2a18424c97db55cd1d9ec9d1
mopeneko <mopeneko@lem0n.cc>
2024-03-24 18:55:58 +0900

    Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-import { vitePlugin as remix } from '@remix-run/dev';
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from '@remix-run/dev';
 import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -6,5 +9,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 installGlobals();
 
 export default defineConfig({
-plugins: [remix(), tsconfigPaths()],
+  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
